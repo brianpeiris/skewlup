dc1ccfece6ae83956f564158d9bf4600035f6fe1
Brian Peiris <brianpeiris@gmail.com>
2023-04-21 14:46:33 -0400

    Allow dupe tags for multiple cities

--- src/models/Tag.ts
+++ src/models/Tag.ts
@@ -2,6 +2,6 @@ import { DataTypes } from "sequelize";
 import sequelize from "../lib/db.js";
 
 export default sequelize.define("Tag", {
-  tag: { type: DataTypes.STRING, allowNull: false, unique: true },
+  tag: { type: DataTypes.STRING, allowNull: false },
   count: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
 });
