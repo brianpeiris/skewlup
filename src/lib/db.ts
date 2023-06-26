2ba5e048ff1645c426ca403cafc2efd43dcdfb7f
Brian Peiris <brianpeiris@gmail.com>
2023-06-25 22:27:40 -0400

    configurable db name

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,8 +3,8 @@ dotenv.config();
 
 import { Sequelize } from "sequelize";
 
-const { DB_HOST, DB_PASSWORD } = process.env;
+const { DB_HOST, DB_PASSWORD, DB_NAME } = process.env;
 
 export default new Sequelize(
-  `postgres://postgres:${DB_PASSWORD}@${DB_HOST}:5432/skewlup`
+  `postgres://postgres:${DB_PASSWORD}@${DB_HOST}:5432/${DB_NAME}`
 );
